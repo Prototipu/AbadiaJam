30b8c185e25e10bbb7f92fde0df20335db7dd4c0
Prototipu <carloscarbobalaguer@gmail.com>
2024-02-11 16:35:20 +0100

    todosloscambios

---
+++ SlowJam/Assets/Scripts/TooltipInfos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace ChristinaCreatesGames.Typography.TooltipForTMP
+{
+    [Serializable]
+    public struct TooltipInfos
+    {
+        public string Keyword;
+        [TextArea]
+        public string Description;
+        public Sprite Image;
+    }
+}
